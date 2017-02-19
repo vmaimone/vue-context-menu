d95510801e11fe63e1124fe0469a8e63f7855be3
Vin Maimone <vmaimone@rochester.rr.com>
2017-02-19 05:29:24 -0500

    update demo index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-module.exports = require('./index.vue')
+Vue = require("vue")
+Vue.config.debug = true
+Router = require("/Users/vin/@vmaimone/vue-context-menu/node_modules/vue-router/dist/vue-router.common.js")
+Vue.use(Router)
+routes = [
+  {path: "/index", component: require("./index.vue")},
+
+]
+router = new Router({routes:[
+  {path: "/index", component: require("./index.vue")},
+
+  {path:"/",component: require("/Users/vin/@vmaimone/vue-context-menu/node_modules/vue-dev-server/app/main.js")}
+]})
+router.afterEach(function(to) {
+  document.title = to.path + " - vue-dev-server"
+})
+app = new Vue({
+  data: function() {return {availableRoutes: routes}},
+  template: "<router-view></router-view>",
+  router: router
+  }).$mount("#app")
